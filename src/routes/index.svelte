abf3934bf9bacfb0b0f14fa251c4470f1b42a586
lakhansamani <lakhan.m.samani@gmail.com>
2020-10-02 01:07:24 +0530

    remove italic fonts from intro

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <div class="flex justify-center lg:justify-between align-middle flex-wrap-reverse lg:flex-no-wrap">
 	<div class="flex-initial lg:pr-10 lg:w-2/3">
 		<div class="my-5">
-			<div class="text-center lg:text-left italic text-4xl font-bold  text-yellow-500">Hello World 👋</div>
-			<div class="text-center lg:text-left italic text-3xl font-bold text-yellow-300 ">I'm Lakhan Samani</div>
+			<div class="text-center lg:text-left text-4xl font-bold  text-yellow-500">Hello World 👋</div>
+			<div class="text-center lg:text-left text-3xl font-bold text-yellow-300 ">I'm Lakhan Samani</div>
 		</div>
 		<p class="text-center lg:text-justify text-gray-300 text-xl">
 			I'm a software engineer based in Vadodara, India 🇮🇳. With a knack for problem-solving, I help build solutions <b>and</b> I love contributing to open source. I am an independent contractor helping various organizations build their products.
