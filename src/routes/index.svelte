ec894eae695ccb3fcb6cb2bf724156a84fed978c
lakhansamani <lakhan.m.samani@gmail.com>
2020-10-02 01:03:29 +0530

    fixes intro content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,14 @@
-
-
 <div class="flex justify-center lg:justify-between align-middle flex-wrap-reverse lg:flex-no-wrap">
 	<div class="flex-initial lg:pr-10 lg:w-2/3">
-		<div class="text-center lg:text-left italic text-4xl font-bold my-10 text-yellow-500">Hey! I'am Lakhan Samani</div>
+		<div class="my-5">
+			<div class="text-center lg:text-left italic text-4xl font-bold  text-yellow-500">Hello World 👋</div>
+			<div class="text-center lg:text-left italic text-3xl font-bold text-yellow-300 ">I'm Lakhan Samani</div>
+		</div>
 		<p class="text-center lg:text-justify text-gray-300 text-xl">
 			I'm a software engineer based in Vadodara, India 🇮🇳. With a knack for problem-solving, I help build solutions <b>and</b> I love contributing to open source. I am an independent contractor helping various organizations build their products.
 		</p>
 	</div>
 	<div class="flex-initial">
-		<img src="images/profile.jpg" alt="profile" class="rounded-full lg:rounded-md h-64 my-10"/>
+		<img src="images/profile.jpg" alt="profile" class="rounded-full lg:rounded-md h-64 my-5"/>
 	</div>
 </div>
