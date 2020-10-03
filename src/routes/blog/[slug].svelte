ae13387c6b5c35f42813c7905fdcd2485a9f770f
lakhansamani <lakhan.m.samani@gmail.com>
2020-10-03 15:30:16 +0530

    add blog page

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -2,7 +2,7 @@
 	export async function preload({ params }) {
 		// the `slug` parameter is available because
 		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
+		const res = await this.fetch(`https://dev.to/api/articles/lakhansamani/${params.slug}`);
 		const data = await res.json();
 
 		if (res.status === 200) {
@@ -26,13 +26,33 @@
 		so we have to use the :global(...) modifier to target
 		all elements inside .content
 	*/
+
+	.content :global(pre) {
+		margin: 1.25rem 0;
+	}
+
+	.content :global(p) {
+		margin-bottom: 2.5rem;
+	}
+
+	.content :global(a) {
+		color: var(--text-link);
+	}
+
+	.content :global(h3) {
+		font-size: 1.5rem;
+		margin-bottom: 5px;
+		font-weight: bolder;
+	}
+
 	.content :global(h2) {
-		font-size: 1.4em;
-		font-weight: 500;
+		font-size: 1.8rem;
+		margin-bottom: 5px;
+		font-weight: bolder;
 	}
 
 	.content :global(pre) {
-		background-color: #f9f9f9;
+		background-color: #133b5c;
 		box-shadow: inset 1px 1px 5px rgba(0, 0, 0, 0.05);
 		padding: 0.5em;
 		border-radius: 2px;
@@ -54,11 +74,26 @@
 </style>
 
 <svelte:head>
-	<title>{post.title} | lakhan.me</title>
+	<title>{post.title} | Lakhan Samani</title>
+	<meta property="og:url" content={`https://www.lakhan.me/blog/${post.slug}`} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={post.title} />
+	<meta name="Description" content={post.description} />
+	<meta property="og:description" content={post.description} />
+	<meta property="og:image" content="https://www.lakhan.me/images/profile.jpg" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content="https://twitter.com/lakhansamani/" />
+	<meta name="twitter:title" content={post.title} />
+	<meta name="twitter:description" content={post.description} />
+	<meta name="twitter:image" content="https://www.lakhan.me/images/profile.jpg" />
 </svelte:head>
 
-<h1>{post.title}</h1>
-
-<div class="content">
-	{@html post.html}
+<h1 class="text-center text-4xl lg:text-5xl font-bold text-yellow-500 mb-5">{post.title}</h1>
+<ul class="flex flex-wrap">
+	{#each post.tags as tag}
+		<li class="mr-3 mb-1 bg-yellow-300 p-1 text-gray-900 rounded-md text-sm"><code>#{tag}</code></li>
+	{/each}
+</ul>
+<div class="content text-xl">
+	{@html post.body_html}
 </div>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export function preload() {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+		return this.fetch(`https://dev.to/api/articles?username=lakhansamani`).then(r => r.json()).then(posts => {
 			return { posts };
 		});
 	}
@@ -18,10 +18,18 @@
 </style>
 
 <svelte:head>
-	<title>Blog</title>
+	<meta property="og:url" content="https://www.lakhan.me/blog/" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="Blog | Lakhan Samani" />
+	<meta name="Description" content="Lakhan Samani's blog posts" />
+	<meta property="og:description" content="Lakhan Samani's blog posts" />
+	<meta property="og:image" content="https://www.lakhan.me/images/profile.jpg" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content="https://twitter.com/lakhansamani/" />
+	<meta name="twitter:title" content="Blog | Lakhan Samani" />
+	<meta name="twitter:description" content="Lakhan Samani's blog posts" />
+	<meta name="twitter:image" content="https://www.lakhan.me/images/profile.jpg" />
 </svelte:head>
-
-<h1>Recent posts</h1>
 
 <ul>
 	{#each posts as post}
@@ -29,6 +37,18 @@
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel="prefetch" href="blog/{post.slug}">{post.title}</a></li>
+		<li class="mb-10 border-yellow-300 rounded-md p-5 border-2">
+			<a rel="prefetch" href="blog/{post.slug}">
+				<h2 class="text-xl font-bold text-yellow-500">{post.title}</h2>
+				<p class="text-lg mt-1 mb-5">
+					{post.description}
+				</p>
+				<ul class="flex flex-wrap">
+					{#each post.tag_list as tag}
+						<li class="mr-3 mb-1 bg-yellow-300 p-1 text-gray-900 rounded-md text-sm"><code>#{tag}</code></li>
+					{/each}
+				</ul>
+			</a>
+		</li>
 	{/each}
 </ul>
