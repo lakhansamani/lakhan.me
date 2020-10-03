b839f8aea672ee3d0142506dbdc1f88856da2ca8
lakhansamani <lakhan.m.samani@gmail.com>
2020-10-03 13:42:00 +0530

    add project details in about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,16 +1,27 @@
 <svelte:head>
 	<title>About Lakhan </title>
+	<meta property="og:url" content="https://www.lakhan.me/about/" />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="About | Lakhan Samani" />
+	<meta name="Description" content="About Lakhan Samani's career, skills and projects" />
+	<meta property="og:description" content="About Lakhan Samani's career, skills and projects" />
+	<meta property="og:image" content="https://www.lakhan.me/images/profile.jpg" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content="https://twitter.com/lakhansamani/" />
+	<meta name="twitter:title" content="About | Lakhan Samani" />
+	<meta name="twitter:description" content="About Lakhan Samani's career, skills and projects" />
+	<meta name="twitter:image" content="https://www.lakhan.me/images/profile.jpg" />
 </svelte:head>
 
 <h1 class="text-center text-4xl lg:text-5xl font-bold text-yellow-500 mb-8">About Lakhan</h1>
 
 <div class="text-xl">
 	<p class="mb-10">
-		I am a Software Engineer with a love for solving problems. I believe <a href="https://en.wikipedia.org/wiki/Programming_language" target="_blank" rel="noopener" class="text-link">programming languages</a> are the syntax that can help you communicate with digital systems (computer machine/web/mobile). Languages can come and go, but the thing that will remain is your ability to solve problems. At the same time, it is very important to keep yourself updated with new tools/technologies because that will give more power to solve the problems more efficiently. I have a Masters degree in Information and Technology from <a href="https://en.wikipedia.org/wiki/Programming_language" target="_blank" rel="noopener" class="text-link">Dhirubhai Ambani Institute of Information and Technology</a>. But this doesn't change the fact, that I enjoy learning new tools and technologies and keep improving myself. I try to learn and share my learning through this website.
+		I am a Software Engineer with a love for solving problems 😍. I believe <a href="https://en.wikipedia.org/wiki/Programming_language" target="_blank" rel="noopener" class="text-link">programming languages</a> are the syntax that can help you communicate with digital systems (computer machine/web/mobile). Languages can come and go, but the thing that will remain is your ability to solve problems. At the same time, it is very important to keep yourself updated with new tools/technologies because that will give more power to solve the problems more efficiently. I have a Masters degree in Information and Technology from <a href="https://en.wikipedia.org/wiki/Programming_language" target="_blank" rel="noopener" class="text-link">Dhirubhai Ambani Institute of Information and Technology</a>. But this doesn't change the fact, that I enjoy learning new tools and technologies and keep improving myself. I try to learn and share my learning through this website.
 	</p>
 
 	<p class="mb-10">
-		I am best known for tooling, building websites, mobile apps, and handling the deployments.
+		I am best known for tooling, building websites, mobile apps, and handling the deployments. I focus on delivering good quality products at a good pace. At the same time, I follow the idiomatic practices for the development and do not compromise on developer experience.
 	</p>
 
 	<h3 class="mb-3 text-2xl font-bold">What I'm Doing now?</h3>
@@ -47,16 +58,37 @@
 
 	<h3 class="mb-3 text-2xl font-bold">Projects</h3>
 	<div class="mb-10">
-		Dejavu V2, an open-source tool to easily view/access and perform various operations on Elasticsearch data.
-		Arc a reverse proxy to Elasticsearch which gives search relevancy, analytics, and security out of the box.
-		Appbase.io dashboard which allows user to provision Elasticsearch cluster, deploy Arc and manage various tasks with respect to search
-		Heroku deployment for Appbase.io
-		Docker deployment for Appbase.io
-		Kubernetes deployment for Appbase.ip
-		Contributing and maintaining ReactiveSearch, a component library for providing various search facets into your application, which have Elasticsearch as their data source.
-		Importer, a tool for importing data from different sources into Appbase apps
-		The Contentment Foundation web platform:
-		The Contentment Foundation mobile application:
-		
+		<ul class="list-disc">
+			<li class="mb-5"><a href="https://github.com/appbaseio/dejavu" target="_blank" rel="noopener" class="text-link">Dejavu</a> an open-source tool to easily view/access and perform various operations on Elasticsearch data.</li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/arc" target="_blank" rel="noopener" class="text-link">Arc</a> a reverse proxy to Elasticsearch which gives search relevancy, analytics, and security out of the box.</li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/dashboard" target="_blank" rel="noopener" class="text-link">Appbase.io dashboard</a> which allows user to provision Elasticsearch cluster, deploy Arc and manage various tasks with respect to search. Here I have also developed backend APIs which will allow user to provision clusters and perform various operations securely.</li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/appbaseio-heroku" target="_blank" rel="noopener" class="text-link">Appbase.io Heroku Deployment</a> Allows you to deploy Appbase.io services in one click on <a href="https://www.heroku.com/" target="_blank" rel="noopener" class="text-link">Heroku</a></li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/arc-dockerized" target="_blank" rel="noopener" class="text-link">Docker deployment for Appbase.io</a> Allows you to deploy Appbase.io services on docker container</li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/arc-k8s" target="_blank" rel="noopener" class="text-link">Kubernetes deployment for Appbase.ip</a> Allows you to deploy Appbase.io services with Kubernetes orchestration</li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/arc-k8s" target="_blank" rel="noopener" class="text-link">ReactiveSearch:</a> UI components library for Elasticsearch: Available for React, Vue and React Native</li>
+
+			<li class="mb-5"><a href="https://importer.appbase.io/" target="_blank" rel="noopener" class="text-link">Importer</a> GUI for importing data from different sources into Elasticsearch</li>
+
+			<li class="mb-5"><a href="https://github.com/appbaseio/abc" target="_blank" rel="noopener" class="text-link">ABC</a> CLI for importing data from different sources into Elasticsearch</li>
+
+			<li class="mb-5"><a href="https://github.com/lakhansamani/babel-plugin-reduce-with-operator" target="_blank" rel="noopener" class="text-link">babel-plugin-reduce-with-operator</a>: Babel plugin to reduce an array with just passing operator without writing any callback. This is just an experiment plugin developed while I was learning <a href="https://en.wikipedia.org/wiki/Abstract_syntax_tree" target="_blank" rel="noopener" class="text-link">AST</a></li>
+
+			<li class="mb-5"><a href="https://github.com/lakhansamani/react-minimal-ts-boilerplate" target="_blank" rel="noopener" class="text-link">react-minimal-ts-boilerplate</a>: A typescript boilerplate for react which does not come with any extra dependencies and allows you to fully customize your react project. There is also a <a href="https://github.com/lakhansamani/react-minimal-boilerplate" target="_blank" rel="noopener" class="text-link">Javascript</a> version of this library available.</li>
+
+			<li class="mb-5"><a href="https://github.com/lakhansamani/express-ts-boilerplate" target="_blank" rel="noopener" class="text-link">express-ts-boilerplate</a>: A typescript boilerplate for kick starting <a href="https://expressjs.com/" target="_blank" rel="noopener" class="text-link">expressjs server</a></li>
+
+			<li class="mb-5"><a href="https://school.contentment.org/school/signup" target="_blank" rel="noopener" class="text-link">The Contentment Foundation web platform</a>: A platform that helps in transforming the wellbeing of your entire school.</li>
+
+			<li class="mb-5"><a href="https://play.google.com/store/apps/details?id=org.contentment.family&hl=en" target="_blank" rel="noopener" class="text-link">The Contentment Foundation mobile application</a>: A mobile application that helps in transforming the wellbeing of your family. This application is available on <a href="https://apps.apple.com/us/app/contentment-families/id1513321262" target="_blank" rel="noopener" class="text-link">ios</a> and <a href="https://apps.apple.com/us/app/contentment-families/id1513321262" target="_blank" rel="noopener" class="text-link">android</a></li>
+
+			<li class="mb-5"><a href=" https://wotnot.io/" target="_blank" rel="noopener" class="text-link">Wotnot Chat Bot:</a>A chat bot widget built using ReactJS</li>
+
+		</ul>
 	</div>
 </div>
