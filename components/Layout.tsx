33539fe98221f8c28856696d459045fb3100211a
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-06-13 23:19:57 +0530

    Add rss link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -28,6 +28,12 @@ const Layout = ({
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        title="Lakhan Samani | Feed"
+        href="https://lakhan.me/feed.xml"
+      />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={description} />
